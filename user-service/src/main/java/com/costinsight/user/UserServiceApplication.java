7cc7c567c3f739eda20959cb33e7d8885cb74aaa
chiperman <chipermanju@gmail.com>
2025-08-30 10:55:49 +0800

    refactor: Integrate user-service with Nacos

--- user-service/src/main/java/com/costinsight/user/UserServiceApplication.java
+++ user-service/src/main/java/com/costinsight/user/UserServiceApplication.java
@@ -2,10 +2,10 @@ package com.costinsight.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // 暂时注释掉 Nacos 相关注解
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-// @EnableDiscoveryClient // 暂时注释掉 Nacos 相关注解
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
